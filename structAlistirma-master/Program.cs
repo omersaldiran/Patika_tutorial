1c6e0160032000f3ebc5f92941d9fb550b820dfa
Ömer Saldıran <osaldiran@gmail.com>
2022-04-14 11:19:56 +0300

    Add files via upload

---
+++ structAlistirma-master/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Dikdortgen dortgen = new Dikdortgen();
+dortgen.kisaKenar = 4;
+dortgen.uzunKenar = 2;
+System.Console.WriteLine("Alan : " + dortgen.AlanHesapla());
+
+DikdortgenStruct yeni = new DikdortgenStruct();
+
+yeni.kisaKenar = 3;
+yeni.uzunKenar = 5;
+System.Console.WriteLine("Alan Struct : "+yeni.AlanHesapla());
+
+System.Console.WriteLine(Gunler.Cumartesi);
+
+int sicaklik = 42;
+if(sicaklik <= (int)HavaDurumu.Normal){
+    System.Console.WriteLine("Dışarı çıkma, hava soğuk");
+}
+else if (sicaklik <= (int)HavaDurumu.Sicak){
+    System.Console.WriteLine("Hadi dışarı");
+}
+else if (sicaklik >= (int)HavaDurumu.CokSicak){
+    System.Console.WriteLine("Çok sıcak, otur evinde bea!");
+}
+
+
+class Dikdortgen{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long AlanHesapla(){
+        return this.kisaKenar * this.uzunKenar;
+    }
+}
+
+struct DikdortgenStruct
+{
+    public int kisaKenar;
+    public int uzunKenar;
+
+    public long AlanHesapla(){
+        return this.kisaKenar * this.uzunKenar;
+    }
+}
+
+enum Gunler{
+    Pazartesi = 1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu{
+    Soguk = 5,
+    Normal = 20,
+    Sicak = 25,
+    CokSicak=40
+}
---
+++ switchCase-master/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+int month = DateTime.Now.Month;
+
+switch(month){
+    case 1:
+    System.Console.WriteLine("Ocak ayındayız.");
+    break;
+    case 2:
+    System.Console.WriteLine("Şubat ayındayız.");
+    break;
+    case 3:
+    System.Console.WriteLine("Mart ayındayız.");
+    break;
+    case 4:
+    System.Console.WriteLine("Nisan ayındayız.");
+    break;
+    case 5:
+    System.Console.WriteLine("Mayıs ayındayız.");
+    break;
+}
+
+for(int i =0;i<10;i++){
+    if(i%2==0)
+        continue;
+    System.Console.WriteLine("Tek sayılar : " + i);
+}
+
+System.Console.WriteLine("Toplanacak sayıları girin");
+int sayi = int.Parse(Console.ReadLine());
+int sayac = 1;
+int toplam = 0;
+while(sayac<=sayi){
+    toplam +=sayac;
+    sayac++;
+
+}
+
+System.Console.WriteLine(toplam/sayi);
+
+
+char character = 'a';
+while(character<='z'){
+    Console.Write(character);
+    character++;
+}
+
+string[] arabalar = {"BMW","MB","Ford","Seat","Vw"};
+foreach (var item in arabalar)
+{
+    System.Console.WriteLine(item);
+} 
