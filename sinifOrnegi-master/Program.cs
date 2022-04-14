b36fdb559085439fd9cb6aee442392b67c89011c
Ömer Saldıran <osaldiran@gmail.com>
2022-04-14 11:18:40 +0300

    Add files via upload

---
+++ sinifOrnegi-master/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Calisan calisan2 = new Calisan("ahmet","veli",34,"Teknik Servis");
+Calisan calisan3 = new Calisan("Sefa","veli");
+Calisan calisan1 = new Calisan();
+calisan1.adi="Ömer";
+calisan1.soyadi="Saldıran";
+calisan1.no=1;
+calisan1.departman="IT";
+calisan1.EkranaYazdir();
+calisan2.EkranaYazdir();
+calisan3.EkranaYazdir();
+
+
+
+class Calisan
+{
+    public string adi;
+    public string soyadi;
+    public int no;
+    public string departman;
+
+    public Calisan(string adi, string soyadi, int no, string departman){
+        this.adi = adi;
+        this.soyadi = soyadi;
+        this.no = no;
+        this.departman = departman;
+    }
+    public Calisan(string adi, string soyadi){
+        this.adi = adi;
+        this.soyadi = soyadi; 
+    }
+    public Calisan(){}
+
+    public void EkranaYazdir(){
+        System.Console.WriteLine("Çalışanın Adı : {0}",adi);
+        System.Console.WriteLine("Çalışanın Soyadı : {0}",soyadi);
+        System.Console.WriteLine("Çalışanın Numarası : {0}",no);
+        System.Console.WriteLine("Çalışanın Departmanı : {0}",departman);
+        System.Console.WriteLine("***********");
+    }
+}
---
+++ stringSinifiMetodlari-master/Program.cs
@@ -0,0 +1,31 @@
+﻿// String sınıfı metodları
+string degisken = "Merhaba, deneme örnek stringi";
+//Lenght
+System.Console.WriteLine("String'in uzunluğu " + degisken.Length);
+
+//ToUpper , ToLower
+System.Console.WriteLine(degisken.ToUpper() + " " + degisken.ToLower());
+
+//Concat 
+System.Console.WriteLine(String.Concat(degisken," * Bu kısım sonradan eklendi."));
+
+//Compare CompareTo
+System.Console.WriteLine(degisken.CompareTo("mesela nasıl?"));
+
+//Contains
+System.Console.WriteLine(degisken.Contains("Merhaba"));
+System.Console.WriteLine(degisken.StartsWith('M'));
+System.Console.WriteLine(degisken.EndsWith('s'));
+
+//Indexof
+System.Console.WriteLine(degisken.IndexOf("deneme"));
+System.Console.WriteLine(degisken.LastIndexOf('i'));
+
+//Insert
+System.Console.WriteLine(degisken.Insert(0,"Hi, "));
+
+//Padleft, Padright
+System.Console.WriteLine("Sonradan eklenen "+ degisken.PadLeft(40));
+
+//Substring
+System.Console.WriteLine(degisken.Substring(5));
